f3018b67dd6d37f14145aef48f09e255b0f40eef
BadienVanessa <badienvan@gmail.com>
2023-02-08 10:37:10 +0100

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA.jsx'
+import Me from '../../assets/me.png'
+import HeaderSocial from './HeaderSocials.jsx'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>BADIEN VANESSA</h1>
+        <h5 className="text-light">Fullstack Developper</h5>
+        <CTA />
+        <HeaderSocial />
+
+        <div className="me">
+          <img src={Me} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll down</a>
+      </div>
+    </header>
   )
 }
 
